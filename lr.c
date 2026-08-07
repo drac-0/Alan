98c07c74168e6b9f78aa1d0097268fd90381e60d
drac-0 <muhammadfaiz050607@gmail.com>
2026-08-08 07:54:35 +0800

    Test

---
+++ lr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+#define row 
+#define column
+
+#define Ligre(r,c, Parr)\
+      
+
+#define c2;
+
+
+int main(){
+      int arrp[5][2] = {
+            {1,3},
+            {2,4},
+            {3,7},
+            {4,8},
+            {5,10}
+      };
+      
+}
