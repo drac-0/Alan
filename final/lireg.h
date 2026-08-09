4e5e6a817219de563e435a151080297b481e2043
drac-0 <muhammadfaiz050607@gmail.com>
2026-08-09 11:17:50 +0800

    header essence

---
+++ final/lireg.h
@@ -0,0 +1,22 @@
+#ifndef lireg
+#define lireg
+
+#include <fcntl.h>
+#include <stdio.h>
+
+typedef struct{
+      int x;
+      int y;
+}csvDat;
+
+float Slope(int XYsum, int Xsum, int Ysum, int SXsum);
+
+float Bias(int XYsum, int Xsum, int Ysum, int SXsum);
+
+float Ligre(int XYsum, int Xsum, int Ysum, int SXsum, int input);
+
+csvDat * read(FILE *csvFile);
+
+
+#endif // !lireg
+#define lireg
