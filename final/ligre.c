bf994ca7420d574810a1e10026aba9fa337f95c3
drac-0 <muhammadfaiz050607@gmail.com>
2026-08-09 11:17:21 +0800

    essence

---
+++ final/ligre.c
@@ -0,0 +1,35 @@
+#include "lireg.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+float Slope(int XYsum, int Xsum, int Ysum, int SXsum){
+      return (float)((5 * XYsum) - (Xsum * Ysum)) / (float)((5 * SXsum) - (Xsum * Xsum));
+}
+
+float Bias(int XYsum, int Xsum, int Ysum, int SXsum){
+      float CalSlope = Slope(XYsum, Xsum, Ysum, SXsum);
+      return (((float)Ysum - ((float)Xsum * CalSlope))) / 5;
+}
+
+float Ligre(int XYsum, int Xsum, int Ysum, int SXsum, int input){
+      return Slope(XYsum, Xsum, Ysum, SXsum) * input + Bias(XYsum, Xsum, Ysum, SXsum);
+}
+
+csvDat * read(FILE * csvFile){
+      csvDat * Daf = malloc(sizeof(csvDat) * 2048);
+
+      int read = 0;
+      int records = 0;
+
+      do {
+            read = fscanf(csvFile, "%d, %f\n",
+                          &Daf[records].x,
+                          &Daf[records].y
+                          );
+
+            if (read == 2) records++;
+      }while (feof(csvFile));
+      
+      return Daf;
+}
+
