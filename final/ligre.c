8de077482ab78142947778f8610a5332abc93019
drac-0 <muhammadfaiz050607@gmail.com>
2026-08-21 07:30:11 +0800

    i have class in hour and planned to mentally code this there

--- final/ligre.c
+++ final/ligre.c
@@ -1,4 +1,9 @@
 #include "lireg.h"
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 float Slope(float XYsum, int Xsum, float Ysum, int SXsum, int length){
       return (float)((length * XYsum) - (Xsum * Ysum)) / (float)((length * SXsum) - (Xsum * Xsum));
@@ -28,7 +33,43 @@ mainData * readCSV(FILE * csvFile){
 
             if (read == 2) records++;
 
+            else if (read == 0 ) {
+                  fscanf(csvFile, "%*[^\n]\n"); // eat the line
+            }
+
       }while (!feof(csvFile));
+
+      Act->length = records;
+      Act->Data = Daf;
+
+      return Act;
+}
+
+mainData * readCSVpy(char * path){
+      FILE * fptr = fopen(path, "r");
+      csvDat * Daf = malloc(sizeof(csvDat) * 2048);
+      mainData * Act = malloc(sizeof(mainData));
+      if (fptr == NULL){
+            printf("Failed to open");
+            return Act;
+      }
+
+      int read = 0;
+      int records = 0;
+
+      do {
+            read = fscanf(fptr, "%d, %f\n",
+                          &Daf[records].x,
+                          &Daf[records].y
+                          );
+
+            if (read == 2) records++;
+
+            else if (read == 0 ) {
+                  fscanf(fptr, "%*[^\n]\n"); // eat the line
+            }
+
+      }while (!feof(fptr));
 
       Act->length = records;
       Act->Data = Daf;
@@ -58,15 +99,32 @@ float MAE(float Slope, float Bi, mainData *evaluated){
             float pred = (Slope * evaluated->Data[i].x + Bi);
             SAE += fabs((evaluated->Data[i].y - pred));
       }
+
       return SAE / evaluated->length;
 }
 
+float MAE_FuckUnion_FuckFABS(float Slope, float Bi, mainData *evaluated){
+      float SAE = 0;
+      u32t temp;
 
+      for (int i = 0 ; i < evaluated->length; i++){
+            float E = (evaluated->Data[i].y - (Slope * evaluated->Data[i].x + Bi));
+            temp = * (u32t * ) &E;
+            temp &= 0x7FFFFFFF;
+            E = * (float *) &temp;
+            SAE += E;
+      }
 
-float absolutefloat(){
-
+      return SAE / evaluated->length;
 }
 
+mainData * Init(){
+      csvDat * csvDat = calloc(sizeof(csvDat), BUFSIZ);
+      mainData * ThisIsAllIhave =  calloc(sizeof(mainData), 1);
+      unsigned int length = 0;
 
+      ThisIsAllIhave->length = length;
+      ThisIsAllIhave->Data = csvDat;
 
-
+      return ThisIsAllIhave;
+}
